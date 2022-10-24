872418e885f5a067d1c716d89a223e41321d4311
Constantin <konstantindanilov19@hotmail.com>
2022-10-24 13:29:51 +0300

    started working on the project. Created esling.confid, created SignIn component, did html for SignIn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div className="App">
+    <SignIn/>
+  </div>;
+};
 
 export default App;
+
+const SignIn: React.FC = () => {
+  return (
+    <div className='container'>
+      <div className='sign_in_wrapper'>
+        <form action="">
+          <h1 className='sign_in_heading'>Sign In</h1>
+          <p>
+            <label className='sign_in_label' htmlFor="username">Login</label>
+            <input className='sign_in_input' id='username' type="text" />
+          </p>
+          <p>
+            <label className='sign_in_label' htmlFor="username">Password</label>
+            <input className='sign_in_input' id='username' type="text" />
+          </p>
+          <p>
+            <input className="sign_in_submit" type='submit' value='Sign In'></input>
+          </p>
+          <p>
+            Not a member yet? 
+            <a className='sign_in_sign_up' href="#">Sign up</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};

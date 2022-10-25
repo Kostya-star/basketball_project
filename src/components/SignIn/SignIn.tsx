5039d4288869ece8a61c308538562b86ba983ad1
Constantin <konstantindanilov19@hotmail.com>
2022-10-25 12:59:11 +0300

    validated the login input

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,32 +1,60 @@
-/* eslint-disable @typescript-eslint/restrict-plus-operands */
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import s from './SignIn.module.scss';
 import signInImg from '../../assets/img/imgSignIn/signin-img.png';
 // import eyePasswordSvg from '../../assets/img/imgSignIn/close_eye.svg';
 import { SignInPassword } from './SignInPassword/SignInPassword';
 
 export const SignIn: React.FC = () => {
+
+  const [loginInput, setLoginInput] = React.useState('');
+  const [loginInputDirty, setLoginInputDirty] = React.useState(false);
+  const [loginInputError, setLoginInputError] = React.useState("Can't be empty!");
+  const [submitBtnEnabled, setSubmitBtnEnabled] = React.useState(false)
+
+  React.useEffect(() => {
+    if  (loginInputError) setSubmitBtnEnabled(false)
+    else setSubmitBtnEnabled(true)
+  }, [loginInputError])
+
+  const onInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setLoginInput(e.target.value);
+    const loginRegex = /^[a-zA-Z0-9]+$/;
+    if (!loginRegex.test(String(e.target.value).toLowerCase())) {
+      setLoginInputError('incorrect login');
+      if (e.target.value.length === 0) {
+        setLoginInputError("Can't be empty!");
+      }
+    } else setLoginInputError('');
+  };
+
+  const onBlurHandler = () => {
+    setLoginInputDirty(true);
+  };
+
   return (
     <div className={s.signIn__wrapper}>
       <div className={s.signIn__form}>
         <form action="">
           <h1 className={s.signIn__form__heading}>Sign In</h1>
-          <p className={s.signIn__form__groupOne}>
-            <label className={s.signIn__form__label} htmlFor="username">
-              Login
-            </label>
-            <input className={s.signIn__form__input} id="username" type="text" />
-          </p>
+          <label className={s.signIn__form__label} htmlFor="username">
+            Login
+            {loginInputDirty && loginInputError !== null && (
+              <div style={{ color: 'red' }}>{loginInputError}</div>
+            )}
+            <input
+              className={s.signIn__form__input}
+              id="username"
+              type="text"
+              value={loginInput}
+              onChange={(e) => onInputHandler(e)}
+              onBlur={onBlurHandler}
+            />
+          </label>
+
+          <SignInPassword />
 
           <p>
-            <label className={s.signIn__form__label} htmlFor="username">
-              Password
-            </label>
-            <SignInPassword />
-          </p>
-          
-          <p>
-            <input className={s.signIn__form__submit} type="submit" value="Sign In"></input>
+            <input disabled={!submitBtnEnabled} className={s.signIn__form__submit} type="submit" value="Sign In"></input>
           </p>
           <p>
             <span className={s.signIn__form__span}>

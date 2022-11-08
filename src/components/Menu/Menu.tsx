ba64d19936da2f7598e3ca7d9e6b18ce1fab8764
Constantin <konstantindanilov19@hotmail.com>
2022-11-08 17:34:47 +0200

    started working on the menu buttons

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,24 @@
-import { Players } from './Players/Players';
-import { SignOut } from './SignOut/SignOut';
-import { Teams } from './Teams/Teams';
-import '../../scss/menu-common.scss';
+import { useState } from 'react'
+import { Players } from './Players/Players'
+import { SignOut } from './SignOut/SignOut'
+import { Teams } from './Teams/Teams'
+import '../../scss/menu-common.scss'
+
+
+export interface IMenuButtonProps {
+  isActive: boolean
+  setIsActive: (isActive: boolean) => void
+}
+
 
 export const Menu = () => {
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <div className="menu__wrapper">
       <div className="menu__group">
-        <Teams />
-        <Players />
+        <Teams isActive={isActive} setIsActive={setIsActive}/>
+        <Players isActive={isActive} setIsActive={setIsActive}/>
       </div>
       <SignOut />
     </div>
--- src/components/Menu/Players/Players.tsx
+++ src/components/Menu/Players/Players.tsx
@@ -1,11 +1,17 @@
+import { FC } from 'react';
 import { ReactComponent as PlayersSVG } from '../../../assets/icons/menu__players.svg';
 import '../../../scss/menu-common.scss';
+import classnames from 'classnames';
+import { IMenuButtonProps } from './../Menu';
 
-export const Players = () => {
+export const Players: FC<IMenuButtonProps> = ({ isActive, setIsActive }) => {
   return (
     <>
       <div className="menu__players">
-        <button type="submit">
+      <button type="submit" className={classnames({
+      'menu__button': !isActive,
+      'menu__button_keepFocus': isActive
+    }) } onClick={() => setIsActive(true)}>
           <PlayersSVG/>
           <span>Players</span>
         </button>
@@ -13,3 +19,4 @@ export const Players = () => {
     </>
   );
 };
+

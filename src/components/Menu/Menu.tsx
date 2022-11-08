2d23b40056501447fd0eda1621ab4767f8ea257e
Constantin <konstantindanilov19@hotmail.com>
2022-11-08 18:14:37 +0200

    recreated meny teams and menu players with .map() and styled both out

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,24 +1,42 @@
-import { useState } from 'react'
-import { Players } from './Players/Players'
-import { SignOut } from './SignOut/SignOut'
-import { Teams } from './Teams/Teams'
-import '../../scss/menu-common.scss'
-
+import { useState } from 'react';
+import { Players } from './Players/Players';
+import { SignOut } from './SignOut/SignOut';
+import { Teams } from './Teams/Teams';
+import '../../scss/menu-common.scss';
+import classnames from 'classnames';
+import { ReactComponent as TeamsSVG } from '../../assets/icons/menu__team.svg';
+import { ReactComponent as PlayersSVG } from '../../assets/icons/menu__players.svg';
 
 export interface IMenuButtonProps {
-  isActive: boolean
-  setIsActive: (isActive: boolean) => void
+  isActive: boolean;
+  setIsActive: (isActive: boolean) => void;
 }
 
-
 export const Menu = () => {
-  const [isActive, setIsActive] = useState(false)
-
+  const [isActive, setIsActive] = useState<boolean | number>(false);
+  const menuCategories = ['Teams', 'Players'];
   return (
     <div className="menu__wrapper">
       <div className="menu__group">
-        <Teams isActive={isActive} setIsActive={setIsActive}/>
-        <Players isActive={isActive} setIsActive={setIsActive}/>
+        {/* <Teams isActive={isActive} setIsActive={setIsActive}/>
+        <Players isActive={isActive} setIsActive={setIsActive}/> */}
+        {menuCategories.map((categoryName, index) => (
+          <div key={index}>
+            <button
+              type="submit"
+              className={classnames({
+                'menu__button': Number(isActive) !== index,
+                'menu__button_keepFocus': Number(isActive) === index
+              }) }
+              onClick={() => setIsActive(index)}
+            >
+              {categoryName === 'Teams' && (<div className='menu__teams'><TeamsSVG /><span>{categoryName}</span></div>)}
+
+              {categoryName === 'Players' && (<><PlayersSVG /><span>{categoryName}</span></>)}
+
+            </button>
+          </div>
+        ))}
       </div>
       <SignOut />
     </div>

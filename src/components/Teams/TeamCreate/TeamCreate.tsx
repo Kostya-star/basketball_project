4a43cf372f190882de24a613b8618cdf9ee12531
Constantin <konstantindanilov19@hotmail.com>
2022-11-18 15:52:36 +0200

    finished working with team_create branch. deleted logic with team_create  img upload from redux and put it localy. thus changed the teamslice a lil bit.

--- src/components/Teams/TeamCreate/TeamCreate.tsx
+++ src/components/Teams/TeamCreate/TeamCreate.tsx
@@ -3,56 +3,60 @@ import { InputText } from '../../FormComponents/InputText';
 import { Form, Formik } from 'formik';
 import { InputSubmit } from '../../FormComponents/InputSubmit';
 import * as Yup from 'yup';
-import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
-import { ChangeEvent, useState, useEffect } from 'react';
-import { createTeam, setTeamImage } from '../../../redux/slices/teamsSlice';
+import { useAppDispatch} from '../../../redux/hooks';
+import { ChangeEvent, useState} from 'react';
+import { createTeam } from '../../../redux/slices/teamsSlice';
 import { InputFile } from '../../FormComponents/InputFile';
-import { Navigate, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { INewTeamValues } from '../../../types/types';
 
 
-const validationSchema = Yup.object({
-  name: Yup.string()
-    .required('Required')
-    .matches(/^[a-zA-Z]+$/, 'Field can only contain Latin letters'),
-  division: Yup.string()
-    .required('Required')
-    .matches(/^[a-zA-Z]+$/, 'Field can only contain Latin letters'),
-  conference: Yup.string()
-    .required('Required')
-    .matches(/^[a-zA-Z]+$/, 'Field can only contain Latin letters'),
-  foundationYear: Yup.string()
-    .required('Required')
-    .matches(/^[0-9]+$/, 'Field can only contain numbers'),
-  imageUrl: Yup.mixed().required('Required')
-});
 
 export const TeamCreate = () => {
   const navigate = useNavigate()
   const dispatch = useAppDispatch();
+  const location = useLocation()
   
-  const { teamImg, imageUrl } = useAppSelector(({ teams }) => ({
-    teamImg: teams.teamImg,
-    imageUrl: teams.imageUrl,
-  }));
-
+  const[teamImage, setTeamImage] = useState<File | null>(null)
+  
   const onSaveTeamPhoto = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files?.length) {
-      void dispatch(setTeamImage(e.target.files[0]));
+    if (e.target.files?.length && location.pathname === '/TeamCreate') {
+      setTeamImage(e.target.files[0])
     }
   };
-
+  
   const onCancelHandle = () => {
     navigate('/TeamsEmpty')
   }
 
-//   function inputVal<T = string>(string: T): T {
-//     return string;
-// }
-// inputVal('userName')
+  const initialValues = {
+    name: '',
+    foundationYear: '',
+    division: '',
+    conference: '',
+    imageUrl: '',
+  } as INewTeamValues
 
-
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .required('Required')
+      .matches(/^[a-zA-Z]+$/, 'Field can only contain Latin letters'),
+    division: Yup.string()
+      .required('Required')
+      .matches(/^[a-zA-Z]+$/, 'Field can only contain Latin letters'),
+    conference: Yup.string()
+      .required('Required')
+      .matches(/^[a-zA-Z]+$/, 'Field can only contain Latin letters'),
+    foundationYear: Yup.string()
+      .required('Required')
+      .matches(/^[0-9]+$/, 'Field can only contain numbers'),
+    imageUrl: Yup.mixed().required('Required')
+  });
+  
+  const onSubmit = async (values: INewTeamValues) => {
+    await dispatch(createTeam(values, teamImage))
+  }
+  
   return (
     <div className={s.team__create}>
       <div className={s.team__create__header}>
@@ -64,21 +68,9 @@ export const TeamCreate = () => {
       </div>
 
       <Formik
-        initialValues={
-          {
-            name: '',
-            foundationYear: '',
-            division: '',
-            conference: '',
-            imageUrl: '',
-          } as INewTeamValues
-        }
+        initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={async (values) => {
-          values.imageUrl = imageUrl
-          console.log(values);
-          await dispatch(createTeam(values))
-        }}
+        onSubmit={onSubmit}
         // validateOnMount
       >
         {(formik) => {
@@ -86,7 +78,7 @@ export const TeamCreate = () => {
             <Form>
               <div className={s.team__create__content}>
                 <div className={s.team__create__image}>
-                  <InputFile<'imageUrl'> name='imageUrl' image={teamImg} formik={formik} onSavePhoto={onSaveTeamPhoto}/>
+                  <InputFile<'imageUrl'> name='imageUrl' image={teamImage} formik={formik} onSavePhoto={onSaveTeamPhoto}/>
                 </div>
 
                 <div>

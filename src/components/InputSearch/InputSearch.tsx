dbffd33575591e734d68148567817b32443229d1
Constantin <konstantindanilov19@hotmail.com>
2022-12-09 15:03:35 +0200

    fixed search input and changed styling

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -2,27 +2,22 @@ import s from './InputSearch.module.scss';
 import { ReactComponent as SearchSVG } from '../../assets/icons/searchSvg.svg';
 import { FC } from 'react';
 
-
 interface InputSearchProps {
-  value: string
-  onChangeInput: (value: string) => void
+  value: string;
+  onChangeInput: (value: string) => void;
 }
 
 export const InputSearch: FC<InputSearchProps> = ({ value, onChangeInput }) => {
-
   return (
-    <div className={s.search__container}>
+    <div className={s.search__input}>
       <input
-        name='search__input'
-        className={s.search__input}
+        name="search__input"
         placeholder="Search..."
-        type="text"
+        type="search"
         value={value}
         onChange={(e) => onChangeInput(e.target.value)}
       />
-      <div className={s.search__svg}>
-        <SearchSVG />
-      </div>
+      <SearchSVG />
     </div>
   );
 };

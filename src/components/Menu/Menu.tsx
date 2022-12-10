477dbbf615f90941324a3b8a7b0d7cea16691670
Constantin <konstantindanilov19@hotmail.com>
2022-12-10 19:25:28 +0200

    fixed menu. fixed pagination

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,73 +1,67 @@
-import { useState, useEffect, FC } from 'react';
-import { SignOut } from './SignOut/SignOut';
+import { useState } from 'react';
 import '../../scss/menu-common.scss';
-import classnames from 'classnames';
-import { ReactComponent as TeamsSVG } from '../../assets/icons/menu__team.svg';
-import { ReactComponent as PlayersSVG } from '../../assets/icons/menu__players.svg';
 import { useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../../redux/hooks';
+import { MenuBtn } from '../MenuBtn/MenuBtn';
 
 export interface IMenuButtonProps {
   isActive: boolean;
   setIsActive: (isActive: boolean) => void;
 }
 
+const clearCache = () => {
+  if ('caches' in window) {
+    caches
+      .keys()
+      .then((names) => {
+        names.forEach((name) => {
+          void caches.delete(name);
+        });
+      })
+      .catch((error) => {
+        alert('Error when signing out');
+        console.log(error);
+      });
+  }
+  window.location.reload();
+};
+
+const menuButtons = ['Teams', 'Players', 'Sign out'];
+
 export const Menu = () => {
-  const [activeCategory, setActiveCategory] = useState<number>(0);
+  const [activeBtn, setActiveBtn] = useState(0);
   const navigate = useNavigate();
 
-  const onToggleRoute = (categoryName: string) => {
-    categoryName === 'Teams' ? navigate('/Teams') : navigate('/Players');
+  const onClickHandle = (index: number) => {
+    setActiveBtn(index);
+    menuButtons[index] === 'Teams' ? navigate('/Teams') : navigate('/Players');
+    if (menuButtons[index] === 'Sign out') {
+      onHandleSignOut();
+    }
   };
 
-  const menuCategories = ['Teams', 'Players'];
+  // SIGNOUT LOGIC
+
+  const onHandleSignOut = () => {
+    window.localStorage.removeItem('isAuth');
+    navigate('/SignIn');
+    window.localStorage.removeItem('TOKEN');
+    clearCache();
+  };
 
   return (
     <div className="menu__wrapper">
       <div className="menu__group">
-        {menuCategories.map((categoryName, index) => (
-          <div className="menu__block" key={categoryName + '_' + index}>
-            <div key={index}>
-              <div>
-                {categoryName === 'Teams' && (
-                  <button
-                    onClick={() => {
-                      setActiveCategory(index);
-                      onToggleRoute(categoryName);
-                    }}
-                    className={classnames({
-                      menu__button: activeCategory !== index,
-                      menu__button_selected: activeCategory === index,
-                    })}
-                    disabled={activeCategory === index}
-                  >
-                    <TeamsSVG />
-                    <span>{categoryName}</span>
-                  </button>
-                )}
-
-                {categoryName === 'Players' && (
-                  <button
-                    onClick={() => {
-                      setActiveCategory(index);
-                      onToggleRoute(categoryName);
-                    }}
-                    className={classnames({
-                      menu__button: activeCategory !== index,
-                      menu__button_selected: activeCategory === index,
-                    })}
-                    disabled={activeCategory === index}
-                  >
-                    <PlayersSVG />
-                    <span>{categoryName}</span>
-                  </button>
-                )}
-              </div>
-            </div>
+        {menuButtons.map((button, index) => (
+          <div className="menu__block" key={button + '_' + index}>
+            <MenuBtn
+              index={index}
+              activeBtn={activeBtn === index}
+              btnName={button}
+              onClickHandle={onClickHandle}
+            />
           </div>
         ))}
       </div>
-      <SignOut />
     </div>
   );
 };
---
+++ src/components/MenuBtn/MenuBtn.tsx
@@ -0,0 +1,28 @@
+import classnames from 'classnames';
+import { FC } from 'react';
+import { ReactComponent as TeamsSVG } from '../../assets/icons/menu__team.svg';
+import { ReactComponent as PlayersSVG } from '../../assets/icons/menu__players.svg';
+import { ReactComponent as SignOutSVG } from '../../assets/icons/menu__signOut.svg';
+import s from './MenuBtn.module.scss';
+
+interface IMenuBtnProps {
+  index: number;
+  activeBtn: boolean;
+  btnName: string;
+  onClickHandle: (index: number) => void;
+}
+
+export const MenuBtn: FC<IMenuBtnProps> = ({ index, activeBtn, btnName, onClickHandle }) => {
+  return (
+    <button
+      onClick={() => onClickHandle(index)}
+      className={classnames(s.menu__button, {
+        [s.menu__button_hovered]: !activeBtn,
+        [s.menu__button_selected]: activeBtn,
+      })}
+    >
+      {btnName === 'Teams' ? <TeamsSVG /> : btnName === 'Sign out' ? <SignOutSVG/> : <PlayersSVG />}
+      <span>{btnName}</span>
+    </button>
+  );
+};

7552c7b2dfb21d3af152b733264eaf33fd8e66ff
Constantin <konstantindanilov19@hotmail.com>
2023-01-10 16:56:24 +0200

    adapted the 404 not found page

--- src/components/NotFoundBlock/NotFoundBlock.tsx
+++ src/components/NotFoundBlock/NotFoundBlock.tsx
@@ -7,7 +7,7 @@ export const NotFoundBlock = () => {
     <div className={s.notFound__wrapper}>
       <div>
         <p className={s.notFound__img}>
-          <img src={notFound} alt="" />
+          <img src={notFound} alt="404 img" />
         </p>
         <h1 className={s.notFound__heading}>Page not found</h1>
         <p className={s.notFound__text}>Sorry, we can’t find what you’re looking for</p>

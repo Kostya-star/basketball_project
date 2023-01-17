dbe9445866664838e2b01bac0c8346e1916d378b
Constantin <konstantindanilov19@hotmail.com>
2023-01-17 11:31:23 +0200

    added basename for browser router

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,10 @@ import { AppRouter } from './components/AppRouter/AppRouter';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppRouter />
     </BrowserRouter>
   );
 };
 
 export default App;
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// import { createProxyMiddleware } from 'http-proxy-middleware';
-
-// export default app => {
-//   app.use(createProxyMiddleware('/Team/GetTeams',
-//     {
-//       target: 'http://dev.trainee.dex-it.ru/api',
-//       changeOrigin: true
-//     }))
-//   app.use(createProxyMiddleware('Image/SaveImage',
-//     {
-//       target: 'http://dev.trainee.dex-it.ru/api',
-//       changeOrigin: true
-//     }))
-//   // app.use(createProxyMiddleware('Auth/SignIn',
-//   //   {
-//   //     target: 'http://dev.trainee.dex-it.ru/api',
-//   //     headers: {
-//   //       'Access-Control-Allow-Origin': 'http://localhost',
-//   //       'Access-Control-Allow-Headers': 'Accept',
-//   //       'Access-Control-Allow-Credentials': true,
-//   //       credentials: true,
-//   //     },
-//   //     changeOrigin: true,
-//   //   }))
-//     // app.use .....
-// }

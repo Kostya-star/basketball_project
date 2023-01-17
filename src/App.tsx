b7d06b7eddc82221579d081b0df8dfd75bf8fc6b
Constantin <konstantindanilov19@hotmail.com>
2023-01-17 11:53:41 +0200

    added one more meta tag into index hyml

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import { AppRouter } from './components/AppRouter/AppRouter';
 
 const App: React.FC = () => {
